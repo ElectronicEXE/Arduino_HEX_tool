9c7bcea06909537d4c2c819c38174fe68b392ffc
ElectronicEXE <114730703+ElectronicEXE@users.noreply.github.com>
2024-01-06 17:46:07 +0200

    Update arduino_hex_tool.cmd

--- AHX/arduino_hex_tool.cmd
+++ AHX/arduino_hex_tool.cmd
@@ -3,7 +3,7 @@ echo ############################################################
 echo ###  CODE BY @Electronic_EXE  aka  Furdui Bogdan Andrei  ###
 echo ############################################################
 echo.
-echo version: 3.0
+echo version: 3.2
 echo.
 echo.
 echo.
@@ -51,7 +51,7 @@ if %chose% == 2 (
     set bvar=-b 19200
 ) 
 
-echo _______________________________com port list_________________________________
+echo _______________________________serial port list_________________________________
 echo #############################################################################
 setlocal enabledelayedexpansion 
 echo.
@@ -59,12 +59,12 @@ for /f "tokens=2 delims=COM:" %%i in ('mode ^| findstr /RC:"\C\O\M[0-9*]"') do s
 echo.
 echo.
 echo.
-echo _________________________________set com port________________________________
+echo _________________________________set serial port________________________________
 echo #############################################################################
 echo.
 set /p input=
 echo.
-echo com port is: %input%
+echo serial port is: %input%
 echo.
 echo.
 echo.
@@ -162,13 +162,55 @@ echo.
 echo.
 echo.
 echo.
-echo ______________________________set file name for .hex file___________________
+echo ______________________________output file type___________________
+echo ##############################################################################
+echo.
+echo.
+set count=0
+set folderss=.bin .hex
+
+for %%i in (%folderss%) do (
+    set /a count=count+1
+    set choicee[!count!]=%%i
+)
+
+echo.
+echo select option:
+echo.
+
+for /l %%x in (1,1,!count!) do (
+    echo [%%x] !choicee[%%x]!
+)
+echo.
+
+set /p chosee= 
+echo.
+
+echo You chose !choicee[%chosee%]!
+set folderr=!choicee[%chosee%]!
+
+echo.
+echo.
+echo.
+echo file type is: %chosee%
+if %chosee% == 1 (
+    set type=bin
+)
+
+if %chosee% == 3 (
+    set type=hex
+)
+echo.
+echo.
+echo.
+
+echo ______________________________set file name for .%type% file___________________
 echo ##############################################################################
 echo.
 echo Please place your writing binaries on Desktop if you want to flash them.
 set /p input2=
 echo.
-echo file name for .hex file is: %input2%
+echo file name for .%type% file is: %input2%
 echo.
 echo.
 echo.
@@ -211,17 +253,17 @@ if %chosee% == 2 (
 
 if %chosee% == 1 (
     set input3=w
-    xcopy %USERPROFILE%\Desktop\%input2%.hex %~dp0
+    xcopy %USERPROFILE%\Desktop\%input2%.%type% %~dp0
 )
 echo ##############################################################################
 echo.
 echo command to be executed:
-echo avrdude -p %chip% %cvar% %bvar% -P %input% -U %mode%:%input3%:%input2%.hex:i
+echo avrdude -p %chip% %cvar% %bvar% -P %input% -U %mode%:%input3%:%input2%.%type%:i
 echo.
 echo.
 pause
-avrdude -p %chip% %cvar% %bvar% -P %input% -U %mode%:%input3%:%input2%.hex:i
-xcopy %input2%.hex %USERPROFILE%\Desktop
+avrdude -p %chip% %cvar% %bvar% -P %input% -U %mode%:%input3%:%input2%.%type%:i
+xcopy %input2%.%type% %USERPROFILE%\Desktop
 echo.
 echo.
 echo.
